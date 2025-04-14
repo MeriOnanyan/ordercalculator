13996891a32183cc5ce6018387b4d4f7b8d16ea6
MeriOnanyan <onanyanmeri04@gmail.com>
2025-04-14 12:40:47 +0400

    Move createOrder logic to OrderService (#3)

--- src/main/java/com/myprojects/ordercalculator/controller/OrderController.java
+++ src/main/java/com/myprojects/ordercalculator/controller/OrderController.java
@@ -1,7 +1,6 @@
 package com.myprojects.ordercalculator.controller;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.myprojects.ordercalculator.dto.LineItemRequest;
 import com.myprojects.ordercalculator.dto.OrderRequest;
 import com.myprojects.ordercalculator.dto.OrderUpdateRequest;
-import com.myprojects.ordercalculator.model.LineItem;
 import com.myprojects.ordercalculator.model.Order;
 import com.myprojects.ordercalculator.repository.OrderRepository;
 import com.myprojects.ordercalculator.service.OrderService;
@@ -41,35 +38,9 @@ public class OrderController {
     }
 
     @PostMapping("/create")
-    public Order createOrder(@RequestBody OrderRequest orderRequest) {
-        Order orderItem = new Order();
-
-        orderItem.setName(orderRequest.getName());
-        orderItem.setDescription(orderRequest.getDescription());
-        orderItem.setCreateDate(orderRequest.getCreateDate());
-
-        List<LineItemRequest> lineItemRequests = orderRequest.getLineItemRequests();
-        List<LineItem> lineItems = new ArrayList<>();
-
-        if (lineItemRequests != null){
-            for (int i = 0; i < lineItemRequests.size(); i++) {
-                LineItemRequest lineItemRequestOne = lineItemRequests.get(i);
-                LineItem lineItemOne = new LineItem();
-    
-                lineItemOne.setName(lineItemRequestOne.getName());
-                lineItemOne.setPrice(lineItemRequestOne.getPrice());
-                lineItemOne.setWeight(lineItemRequestOne.getWeight());
-                lineItemOne.setCustomsDuty(lineItemRequestOne.getCustomsDuty());
-                lineItemOne.setOrder(orderItem);
-    
-                lineItems.add(lineItemOne);
-            }
-    
-            orderItem.setLineItems(lineItems);
-    
-        }
-
-        return orderRepository.save(orderItem);
+    public ResponseEntity<Order> createOrder(@RequestBody OrderRequest dto) {
+        Order created = orderService.createOrder(dto);
+        return ResponseEntity.ok(created);
     }
     
     @PutMapping("/{orderId}")
