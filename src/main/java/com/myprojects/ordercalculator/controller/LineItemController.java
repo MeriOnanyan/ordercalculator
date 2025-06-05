89ad912b92d28282b802d2b8111a457a91b31a62
MeriOnanyan <onanyanmeri04@gmail.com>
2025-06-05 10:19:22 +0400

    update an LineItem by ID

--- src/main/java/com/myprojects/ordercalculator/controller/LineItemController.java
+++ src/main/java/com/myprojects/ordercalculator/controller/LineItemController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -71,4 +72,21 @@ public class LineItemController {
             return ResponseEntity.ok("LineItem deleted successfully.");
         }).orElse(ResponseEntity.notFound().build());
     }
+
+    @PutMapping("/{lineItemId}")
+    public ResponseEntity<?> updateLineItem(
+            @PathVariable Long lineItemId,
+            @RequestBody LineItemRequest request
+    ) {
+        return lineItemRepository.findById(lineItemId).map(lineItem -> {
+            lineItem.setName(request.getName());
+            lineItem.setPrice(request.getPrice());
+            lineItem.setWeight(request.getWeight());
+            lineItem.setCustomsDuty(request.getCustomsDuty());
+
+            lineItemRepository.save(lineItem);
+            return ResponseEntity.ok("LineItem updated successfully.");
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
 }
